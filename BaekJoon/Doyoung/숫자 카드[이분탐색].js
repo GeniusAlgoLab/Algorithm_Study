cac9ec9d896720dfd2c89086f9b5ff6edfd05805
Kim Do Young <difbfl4750@gmail.com>
2024-10-21 10:57:38 +0900

    알고리즘 스터디

---
+++ BaekJoon/Doyoung/숫자 카드[이분탐색].js
@@ -0,0 +1,38 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = require("fs").readFileSync(filePath).toString().trim().split("\n");
+
+// 문제
+// 숫자 카드는 정수 하나가 적혀져 있는 카드이다. 상근이는 숫자 카드 N개를 가지고 있다. 정수 M개가 주어졌을 때, 이 수가 적혀있는 숫자 카드를 상근이가 가지고 있는지 아닌지를 구하는 프로그램을 작성하시오.
+
+// 입력
+// 첫째 줄에 상근이가 가지고 있는 숫자 카드의 개수 N(1 ≤ N ≤ 500,000)이 주어진다. 둘째 줄에는 숫자 카드에 적혀있는 정수가 주어진다. 숫자 카드에 적혀있는 수는 -10,000,000보다 크거나 같고, 10,000,000보다 작거나 같다. 두 숫자 카드에 같은 수가 적혀있는 경우는 없다.
+
+// 셋째 줄에는 M(1 ≤ M ≤ 500,000)이 주어진다. 넷째 줄에는 상근이가 가지고 있는 숫자 카드인지 아닌지를 구해야 할 M개의 정수가 주어지며, 이 수는 공백으로 구분되어져 있다. 이 수도 -10,000,000보다 크거나 같고, 10,000,000보다 작거나 같다
+
+// 출력
+// 첫째 줄에 입력으로 주어진 M개의 수에 대해서, 각 수가 적힌 숫자 카드를 상근이가 가지고 있으면 1을, 아니면 0을 공백으로 구분해 출력한다.
+
+const N = parseInt(input[0]);
+const sgCard = input[1].split(" ").map(Number);
+const M = parseInt(input[2]);
+const checkCard = input[3].split(" ").map(Number);
+
+function solution(N, sgCard, M, checkCard) {
+  let result = "";
+  const sgCardMap = new Map();
+  sgCard.forEach((num) => {
+    sgCardMap.set(num, true);
+  });
+
+  checkCard.forEach((num) => {
+    if (sgCardMap.has(num)) {
+      result += "1 ";
+    } else {
+      result += "0 ";
+    }
+  });
+
+  return result;
+}
+
+console.log(solution(N, sgCard, M, checkCard));
