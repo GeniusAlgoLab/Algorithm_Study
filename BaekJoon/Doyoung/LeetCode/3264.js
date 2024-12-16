956c17f61702ee25781a27c61c1f3bd1e44dda90
Kim Do Young <difbfl4750@gmail.com>
2024-12-16 10:46:40 +0900

    알고리즘 스터디

---
+++ BaekJoon/Doyoung/LeetCode/3264.js
@@ -0,0 +1,23 @@
+// You are given an integer array nums, an integer k, and an integer multiplier.
+
+// You need to perform k operations on nums. In each operation:
+
+// Find the minimum value x in nums. If there are multiple occurrences of the minimum value, select the one that appears first.
+// Replace the selected minimum value x with x * multiplier.
+// Return an integer array denoting the final state of nums after performing all k operations.
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number} multiplier
+ * @return {number[]}
+ */
+var getFinalState = function (nums, k, multiplier) {
+  for (let i = 0; i < k; i++) {
+    let min = Math.min(...nums);
+    let minIndex = nums.indexOf(min);
+    nums[minIndex] = min * multiplier;
+  }
+
+  return nums;
+};
