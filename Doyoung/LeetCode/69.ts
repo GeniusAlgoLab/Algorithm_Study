d3a3fc8b91eb9bc219a099e4e94afd838154da1c
Kim Do Young <difbfl4750@gmail.com>
2025-01-20 21:11:53 +0900

    알고리즘 스터디 & 폴더 정리

---
+++ Doyoung/LeetCode/69.ts
@@ -0,0 +1,17 @@
+// Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+// You must not use any built-in exponent function or operator.
+
+// For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+  if (x === 0) return 0;
+
+  let answer = Math.sqrt(x);
+
+  return Math.floor(answer);
+};
